35b2d76bbdc5b656297bb3ee7e25a30665088dde
revdfdev <mkodekar@zoho.com>
2019-10-15 15:38:10 +0530

    Initial commit

---
+++ src/main/kotlin/com/functional/functionalx/FunctionalX/FunctionalXApplication.kt
@@ -0,0 +1,11 @@
+package com.functional.functionalx.FunctionalX
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FunctionalXApplication
+
+fun main(args: Array<String>) {
+	runApplication<FunctionalXApplication>(*args)
+}
---
+++ src/test/kotlin/com/functional/functionalx/FunctionalX/FunctionalXApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.functional.functionalx.FunctionalX
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class FunctionalXApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
