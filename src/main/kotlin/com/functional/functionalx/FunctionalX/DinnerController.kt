94dda47f438e122bf85efef7dfdd0476d6a947fb
MohammadRehan Kodekar <MohdRehan.Kodekar@crossasyst.local>
2019-10-16 15:12:18 +0530

    Add prepare food method in dinner controller

--- src/main/kotlin/com/functional/functionalx/FunctionalX/DinnerController.kt
+++ src/main/kotlin/com/functional/functionalx/FunctionalX/DinnerController.kt
@@ -1,8 +1,13 @@
 package com.functional.functionalx.FunctionalX
 
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class DinnerController {
 
+    @GetMapping("/prepare-food")
+    fun prepareFood() {
+
+    }
 }
